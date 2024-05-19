23a53311759ed79e60e879b97dbbae350a204e80
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-05-19 17:23:05 +0200

    run events in another thread

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -3,9 +3,38 @@ use crossterm::event::{self, KeyCode, KeyEventKind};
 
 use super::{ui::ui, Tui};
 
+struct EventHandler {
+    rx: tokio::sync::mpsc::UnboundedReceiver<KeyCode>,
+}
+
 #[derive(Debug, Default)]
 pub struct App {
     pub left_size: u16,
+}
+
+impl EventHandler {
+    fn new() -> Self {
+        let tick_rate = std::time::Duration::from_millis(250);
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+        tokio::spawn(async move {
+            loop {
+                if event::poll(tick_rate).unwrap() {
+                    if let event::Event::Key(key) = event::read().unwrap() {
+                        if key.kind == KeyEventKind::Press {
+                            let _ = tx.send(key.code);
+                        };
+                    }
+                }
+                println!("I am still here");
+            }
+        });
+
+        EventHandler { rx }
+    }
+
+    async fn next(&mut self) -> Option<KeyCode> {
+        self.rx.recv().await
+    }
 }
 
 #[allow(dead_code)]
@@ -17,27 +46,26 @@ impl App {
     pub async fn run(&mut self) -> Result<()> {
         let mut tui = Tui::new()?;
         tui.enter()?;
-
-        let mut app = App::default();
+        let mut events = EventHandler::new();
         loop {
-            tui.terminal.draw(|f| ui(f, &mut app))?;
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    match key.code {
-                        KeyCode::Char('q') => break,
-                        KeyCode::Right => app.left_size += 1,
-                        KeyCode::Left => {
-                            if app.left_size > 0 {
-                                app.left_size -= 1
-                            }
-                        }
-                        _ => {}
+            tui.terminal.draw(|f| ui(f, self))?;
+            match events.next().await.unwrap() {
+                KeyCode::Char('q') => break,
+                KeyCode::Right => self.left_size += 1,
+                KeyCode::Left => {
+                    if self.left_size > 0 {
+                        self.left_size -= 1
                     }
                 }
+                _ => {}
             }
         }
 
         tui.exit()?;
+        Ok(())
+    }
+
+    async fn events() -> Result<()> {
         Ok(())
     }
 }
