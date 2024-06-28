41e54bac5f5332dde65d088a76b910f5f9deac7f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-06-28 15:40:33 +0200

    add tests inside test module & improve test for copy_dir_all

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,11 +1,9 @@
 use std::{
     env,
-    fs::{self, read_to_string, File},
+    fs::{self, read_to_string},
     io::{self, Write},
     path::Path,
 };
-
-use uuid::Uuid;
 
 use super::Config;
 
@@ -68,6 +66,7 @@ pub fn get_all_snapshot_ids(src: impl AsRef<Path>) -> io::Result<Vec<String>> {
     Ok(result)
 }
 
+// TODO: change usage to use glob
 fn ignored_files(src: impl AsRef<Path>) -> Vec<String> {
     let mut result = Vec::new();
     let path = src.as_ref().join(".setuprsignore");
@@ -86,7 +85,8 @@ pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<
         let entry = entry?;
         let ty = entry.file_type()?;
 
-        // println!("{:?}", ignored_files(&src));
+        println!("IGNORED FILES -> {:?}", ignored_files(&src));
+        println!("ENTRY PATH -> {:?}", &entry.path());
         if ignored_files(&src).contains(&entry.path().to_string_lossy().into_owned()) {
             continue;
         }
@@ -100,132 +100,207 @@ pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<
     Ok(())
 }
 
-#[allow(dead_code)]
-struct Noisy {
-    folder: String,
-}
+#[cfg(test)]
+mod tests {
+    use std::{
+        fs::{self, File},
+        io::Write,
+        path::Path,
+    };
 
-#[allow(dead_code)]
-struct NoisyFile {
-    name: String,
-    content: String,
-}
+    use uuid::Uuid;
 
-impl Noisy {
+    use crate::core::{
+        utils::{
+            copy_dir_all, get_all_snapshot_ids, ignored_files,
+            search_file_create_config_folder_if_not_found,
+        },
+        Config,
+    };
+
     #[allow(dead_code)]
-    fn new() -> Self {
-        let uuid = Uuid::new_v4().to_string();
-        fs::create_dir(&uuid).unwrap();
-        Self { folder: uuid }
+    struct Noisy {
+        folder: String,
+        cleanup: Option<Box<dyn Fn()>>,
     }
 
     #[allow(dead_code)]
-    fn add_file(self, noisy_file: NoisyFile) -> Self {
-        let path = format!("./{}/{}", self.folder, noisy_file.name);
-        let mut file = File::create(path).unwrap();
-        file.write_all(noisy_file.content.as_bytes()).unwrap();
-        self
+    struct NoisyFile {
+        name: String,
+        content: String,
     }
-}
 
-impl Drop for Noisy {
-    fn drop(&mut self) {
-        let _ = fs::remove_dir_all(format!("./{}", self.folder));
+    impl Noisy {
+        fn new() -> Self {
+            let uuid = Uuid::new_v4().to_string();
+            fs::create_dir(&uuid).unwrap();
+            Self {
+                folder: uuid,
+                cleanup: None,
+            }
+        }
+
+        fn add_folder(self, folder_name: String) -> Self {
+            let path = format!("./{}/{}", self.folder, folder_name);
+            fs::create_dir(path).unwrap();
+            self
+        }
+
+        fn overwrite_cleanup(&mut self, closure: Box<dyn Fn()>) {
+            self.cleanup = Some(Box::new(closure));
+        }
+
+        fn add_file(self, noisy_file: NoisyFile) -> Self {
+            let path = format!("./{}/{}", self.folder, noisy_file.name);
+            let mut file = File::create(path).unwrap();
+            file.write_all(noisy_file.content.as_bytes()).unwrap();
+            self
+        }
+
+        fn add_file_non_method(folder: String, noisy_file: NoisyFile) {
+            let path = format!("./{}/{}", folder, noisy_file.name);
+            let mut file = File::create(path).unwrap();
+            file.write_all(noisy_file.content.as_bytes()).unwrap();
+        }
     }
-}
 
-#[test]
-fn should_retrieve_a_vec_of_all_ignored_files() {
-    let Noisy { folder } = &Noisy::new().add_file(NoisyFile {
-        name: ".setuprsignore".to_string(),
-        content: "ignored_file_0\nignored_file_1\nfolder/ignored_file_2".to_string(),
-    });
-    let expected = vec!["ignored_file_0", "ignored_file_1", "folder/ignored_file_2"];
-    assert_eq!(expected, ignored_files(folder));
-}
+    impl Drop for Noisy {
+        fn drop(&mut self) {
+            let _ = fs::remove_dir_all(format!("./{}", self.folder));
 
-#[test]
-fn should_create_folder_and_file() {
-    let Noisy { folder } = &Noisy::new();
+            match &self.cleanup {
+                Some(f) => f(),
+                None => {}
+            }
+        }
+    }
 
-    let config = Config {
-        config_file_path: ".".to_string(),
-        debug_mode: "error".to_string(),
-        snapshots_path: ".".to_string(),
-    };
+    #[test]
+    fn should_retrieve_a_vec_of_all_ignored_files() {
+        let Noisy { folder, cleanup: _ } = &Noisy::new().add_file(NoisyFile {
+            name: ".setuprsignore".to_string(),
+            content: "ignored_file_0\nignored_file_1\nfolder/ignored_file_2".to_string(),
+        });
+        let expected = vec!["ignored_file_0", "ignored_file_1", "folder/ignored_file_2"];
+        assert_eq!(expected, ignored_files(folder));
+    }
 
-    let file = "file.toml".to_string();
+    #[test]
+    fn should_create_folder_and_file() {
+        let Noisy { folder, cleanup: _ } = &Noisy::new();
 
-    search_file_create_config_folder_if_not_found(format!("./{folder}/{file}").as_str(), &config)
+        let config = Config {
+            config_file_path: ".".to_string(),
+            debug_mode: "error".to_string(),
+            snapshots_path: ".".to_string(),
+        };
+
+        let file = "file.toml".to_string();
+
+        search_file_create_config_folder_if_not_found(
+            format!("./{folder}/{file}").as_str(),
+            &config,
+        )
         .unwrap();
 
-    let file: String = fs::read_to_string(format!("./{folder}/{file}")).unwrap();
-    assert_eq!(
-        file,
-        "config_file_path = '.'\ndebug_mode = 'error'\nsnapshots_path = '.'"
-    );
-}
+        let file: String = fs::read_to_string(format!("./{folder}/{file}")).unwrap();
+        assert_eq!(
+            file,
+            "config_file_path = '.'\ndebug_mode = 'error'\nsnapshots_path = '.'"
+        );
+    }
 
-#[test]
-fn should_copy_folder_recurcivilly() {
-    let Noisy { folder } = &Noisy::new();
+    #[test]
+    fn should_copy_folder_recurcivilly() {
+        let Noisy { folder, cleanup: _ } = &Noisy::new();
 
-    let config = Config {
-        config_file_path: ".".to_string(),
-        debug_mode: "error".to_string(),
-        snapshots_path: ".".to_string(),
-    };
+        let config = Config {
+            config_file_path: ".".to_string(),
+            debug_mode: "error".to_string(),
+            snapshots_path: ".".to_string(),
+        };
 
-    let file = "file.toml".to_string();
+        let file = "file.toml".to_string();
 
-    search_file_create_config_folder_if_not_found(format!("./{folder}/{file}").as_str(), &config)
+        search_file_create_config_folder_if_not_found(
+            format!("./{folder}/{file}").as_str(),
+            &config,
+        )
         .unwrap();
 
-    copy_dir_all(folder, "./test_folder_copy").unwrap();
+        copy_dir_all(folder, "./test_folder_copy").unwrap();
 
-    let file: String = fs::read_to_string(format!("./test_folder_copy/{file}")).unwrap();
-    assert_eq!(
-        file,
-        "config_file_path = '.'\ndebug_mode = 'error'\nsnapshots_path = '.'"
-    );
+        let file: String = fs::read_to_string(format!("./test_folder_copy/{file}")).unwrap();
+        assert_eq!(
+            file,
+            "config_file_path = '.'\ndebug_mode = 'error'\nsnapshots_path = '.'"
+        );
 
-    let _ = fs::remove_dir_all("./test_folder_copy");
-}
+        let _ = fs::remove_dir_all("./test_folder_copy");
+    }
 
-#[test]
-fn should_copy_folder_recurcivilly_ignoring_files_of_setuprsignore() {
-    let Noisy { folder } = &Noisy::new().add_file(NoisyFile {
-        name: ".setupignore".to_string(),
-        content: "ignored_file_0\nignored_file_1".to_string(),
-    });
+    #[test]
+    fn should_copy_folder_recurcivilly_ignoring_files_of_setuprsignore() {
+        let noisy = &mut Noisy::new()
+            .add_file(NoisyFile {
+                name: "normalfile".to_string(),
+                content: "".to_string(),
+            })
+            .add_file(NoisyFile {
+                name: "normalfile".to_string(),
+                content: "".to_string(),
+            })
+            .add_file(NoisyFile {
+                name: "ignored_file_0".to_string(),
+                content: "".to_string(),
+            })
+            .add_folder("folder".to_string())
+            .add_file(NoisyFile {
+                name: "folder/ignored_file_1".to_string(),
+                content: "".to_string(),
+            });
 
-    copy_dir_all(folder, "./test_folder_copy").unwrap();
+        let folder = &noisy.folder.clone();
+        Noisy::add_file_non_method(
+            folder.to_owned(),
+            NoisyFile {
+                name: ".setuprsignore".to_string(),
+                content: format!("{folder}/ignored_file_0\n{folder}/folder/ignored_file_1"),
+            },
+        );
 
-    // let file: String = fs::read_to_string(format!("./test_folder_copy/{file}")).unwrap();
-    // assert_eq!(
-    //     file,
-    //     "config_file_path = '.'\ndebug_mode = 'error'\nsnapshots_path = '.'"
-    // );
+        // noisy.overwrite_cleanup(Box::new(move || {
+        //     fs::remove_dir_all("test_folder_copy").unwrap();
+        // }));
 
-    let _ = fs::remove_dir_all("./test_folder_copy");
-}
+        copy_dir_all(folder, "./test_folder_copy").unwrap();
 
-#[test]
-fn should_retrieve_id() {
-    let Noisy { folder } = &Noisy::new();
+        let on_folder = |file: &str| -> String { format!("./test_folder_copy/{file}") };
+        assert!(Path::new(&on_folder("normalfile")).exists());
+        assert!(!Path::new(&on_folder("ignored_file_0")).exists());
+        assert!(!Path::new(&on_folder("folder/ignored_file_1")).exists());
+    }
 
-    let config = Config {
-        config_file_path: ".".to_string(),
-        debug_mode: "error".to_string(),
-        snapshots_path: ".".to_string(),
-    };
+    #[test]
+    fn should_retrieve_id() {
+        let Noisy { folder, cleanup: _ } = &Noisy::new();
 
-    let file = "file.toml".to_string();
+        let config = Config {
+            config_file_path: ".".to_string(),
+            debug_mode: "error".to_string(),
+            snapshots_path: ".".to_string(),
+        };
 
-    search_file_create_config_folder_if_not_found(format!("./{folder}/{file}").as_str(), &config)
+        let file = "file.toml".to_string();
+
+        search_file_create_config_folder_if_not_found(
+            format!("./{folder}/{file}").as_str(),
+            &config,
+        )
         .unwrap();
 
-    let result = get_all_snapshot_ids(folder).unwrap();
-    let expected = vec![format!("{file}")];
-    assert_eq!(result, expected);
+        let result = get_all_snapshot_ids(folder).unwrap();
+        let expected = vec![format!("{file}")];
+        assert_eq!(result, expected);
+    }
 }
