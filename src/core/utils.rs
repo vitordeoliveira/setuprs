d6b99a12fb3254c945a69590489d7ef4efb9538f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-06-29 10:10:17 +0200

    changing oncelock to lazy_static to mut on tests (WIP)

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,11 +1,17 @@
+use glob::Pattern;
+use lazy_static::lazy_static;
 use std::{
-    env,
-    fs::{self, read_to_string},
+    env, fs,
     io::{self, Write},
     path::Path,
+    sync::Mutex,
 };
 
 use super::Config;
+
+lazy_static! {
+    static ref SETUPRSIGNORE: Mutex<Option<Vec<Pattern>>> = Mutex::new(None);
+}
 
 pub fn search_file_create_config_folder_if_not_found(
     folder_path_and_file: &str,
@@ -66,17 +72,43 @@ pub fn get_all_snapshot_ids(src: impl AsRef<Path>) -> io::Result<Vec<String>> {
     Ok(result)
 }
 
-// TODO: change usage to use glob
-fn ignored_files(src: impl AsRef<Path>) -> Vec<String> {
-    let mut result = Vec::new();
-    let path = src.as_ref().join(".setuprsignore");
-    if let Ok(file_content) = read_to_string(path) {
-        for line in file_content.lines() {
-            result.push(line.to_string());
+fn is_ignored(path: &Path) -> bool {
+    let setup = SETUPRSIGNORE.lock().unwrap();
+
+    if let Some(ref ignore_patterns) = *setup {
+        if let Some(path_str) = path.to_str() {
+            println!("PATTERN: {}", path_str);
+            for pattern in ignore_patterns.iter() {
+                if pattern.matches(path_str) {
+                    return true;
+                }
+            }
         }
     }
+    false
+}
 
-    result
+fn load_gitignore_patterns(path: &Path) -> Vec<Pattern> {
+    let path = format!("{}/.setuprsignore", path.display());
+    let mut patterns = Vec::new();
+    if let Ok(lines) = fs::read_to_string(path) {
+        for line in lines.lines() {
+            let trimmed = line.trim();
+            if !trimmed.is_empty() && !trimmed.starts_with('#') {
+                if let Ok(pattern) = Pattern::new(trimmed) {
+                    patterns.push(pattern);
+                }
+            }
+        }
+    }
+    patterns
+}
+
+fn get_or_set_value(new_value: Vec<Pattern>) {
+    let mut setup = SETUPRSIGNORE.lock().unwrap();
+    if setup.is_none() {
+        *setup = Some(new_value);
+    }
 }
 
 pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
@@ -84,10 +116,11 @@ pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<
     for entry in fs::read_dir(&src)? {
         let entry = entry?;
         let ty = entry.file_type()?;
+        get_or_set_value(load_gitignore_patterns(src.as_ref()));
 
-        println!("IGNORED FILES -> {:?}", ignored_files(&src));
-        println!("ENTRY PATH -> {:?}", &entry.path());
-        if ignored_files(&src).contains(&entry.path().to_string_lossy().into_owned()) {
+        println!("ENTRY: {}", entry.path().display());
+        println!("IS IGNORED: {}", is_ignored(&entry.path()));
+        if is_ignored(&entry.path()) {
             continue;
         }
 
@@ -108,15 +141,18 @@ mod tests {
         path::Path,
     };
 
+    use glob::Pattern;
     use uuid::Uuid;
 
     use crate::core::{
         utils::{
-            copy_dir_all, get_all_snapshot_ids, ignored_files,
+            copy_dir_all, get_all_snapshot_ids, is_ignored, load_gitignore_patterns,
             search_file_create_config_folder_if_not_found,
         },
         Config,
     };
+
+    use super::SETUPRSIGNORE;
 
     #[allow(dead_code)]
     struct Noisy {
@@ -175,14 +211,39 @@ mod tests {
         }
     }
 
+    fn set_value(new_value: Vec<Pattern>) {
+        println!("SET_VALUE: {:?}", new_value);
+        let mut setup = SETUPRSIGNORE.lock().unwrap();
+        *setup = Some(new_value);
+    }
+
     #[test]
-    fn should_retrieve_a_vec_of_all_ignored_files() {
+    fn should_return_true_when_file_is_on_ignore() {
         let Noisy { folder, cleanup: _ } = &Noisy::new().add_file(NoisyFile {
             name: ".setuprsignore".to_string(),
             content: "ignored_file_0\nignored_file_1\nfolder/ignored_file_2".to_string(),
         });
-        let expected = vec!["ignored_file_0", "ignored_file_1", "folder/ignored_file_2"];
-        assert_eq!(expected, ignored_files(folder));
+
+        set_value(load_gitignore_patterns(Path::new(folder)));
+
+        assert!(is_ignored(Path::new("ignored_file_0")));
+        assert!(is_ignored(Path::new("ignored_file_1")));
+        assert!(is_ignored(Path::new("folder/ignored_file_2")));
+
+        assert!(!is_ignored(Path::new("file_1")));
+    }
+
+    #[test]
+    fn should_return_false_when_file_is_not_on_ignore() {
+        let Noisy { folder, cleanup: _ } = &Noisy::new().add_file(NoisyFile {
+            name: ".setuprsignore".to_string(),
+            content: "ignored_file_0\nignored_file_1\nfolder/ignored_file_2".to_string(),
+        });
+
+        set_value(load_gitignore_patterns(Path::new(folder)));
+
+        assert!(!is_ignored(Path::new("file_1")));
+        assert!(is_ignored(Path::new("ignored_file_0")));
     }
 
     #[test]
@@ -269,13 +330,15 @@ mod tests {
             },
         );
 
-        // noisy.overwrite_cleanup(Box::new(move || {
-        //     fs::remove_dir_all("test_folder_copy").unwrap();
-        // }));
+        set_value(load_gitignore_patterns(Path::new(folder)));
+        noisy.overwrite_cleanup(Box::new(move || {
+            fs::remove_dir_all("test_folder_copy").unwrap();
+        }));
 
         copy_dir_all(folder, "./test_folder_copy").unwrap();
 
         let on_folder = |file: &str| -> String { format!("./test_folder_copy/{file}") };
+
         assert!(Path::new(&on_folder("normalfile")).exists());
         assert!(!Path::new(&on_folder("ignored_file_0")).exists());
         assert!(!Path::new(&on_folder("folder/ignored_file_1")).exists());
