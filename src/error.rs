402818b2ffb2d5bcabaecd71726c33af60522d9b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-07-01 17:17:41 +0200

    improve error

--- src/error.rs
+++ src/error.rs
@@ -9,21 +9,11 @@ pub enum Error {
     #[error("Unknown Error")]
     Unknown,
 
+    #[error("Missing setuprs init files, please run setuprs init")]
+    MissingBasicInitialization,
+
     #[error("{0}")]
     IOError(#[from] io::Error),
-}
-
-impl Error {
-    pub fn into_message(&self) -> String {
-        match self {
-            Error::Unknown => "Unknown Error".to_string(),
-            Error::IOError(_) => "IO error".to_string(),
-            // Error::IOError(e) => match e.kind() {
-            //     io::ErrorKind::ExecutableFileBusy => "Nicolas".to_string(),
-            //     _ => "REST".to_string(),
-            // },
-        }
-    }
 }
 
 impl Debug for Error {
